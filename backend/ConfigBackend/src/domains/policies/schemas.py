ba5089018dc4351df78822098f9eae7795132527
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-22 17:47:17 -0300

    feat(policy): add policy schemas

---
+++ backend/ConfigBackend/src/domains/policies/schemas.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from src.domains.blocks.schemas import BlockSchema, CreateOrUpdateBlockSchema
+
+
+class GetPolicySchema(BaseModel):
+    id: int
+    name: Optional[str]
+
+
+class PolicySchema(BaseModel):
+    id: int
+    name: Optional[str]
+    flow: List[BlockSchema]
+
+
+class CreatePolicySchema(BaseModel):
+    name: str
+    flow: List[CreateOrUpdateBlockSchema] = []
+
+
+class UpdatePolicySchema(BaseModel):
+    id: int
+    name: Optional[str]
+    flow: List[CreateOrUpdateBlockSchema] = []
+
+
+class FlowValidationError(Enum):
+    MISSING_START_BLOCK = 'Flow is missing a start block.'
+    MORE_THAN_ONE_START_BLOCK = 'Flow has more than one start block.'
+    START_BLOCK_MISSING_NEXT = (
+        'Start block is missing next_block_id or next_block_temp_id.'
+    )
+    CONDITION_BLOCK_MISSING_MINIMUM_RULES = (
+        'Condition block must have at least two rules'
+    )
+    CONDITION_BLOCK_RULES_MISSING_ELSE = (
+        'Condition block rules must contain ELSE rule'
+    )
+    CONDITION_BLOCK_RULES_WITH_MORE_THAN_ONE_ELSE = (
+        'Condition block rules must contain only one ELSE rule'
+    )
+    CONDITION_BLOCK_RULE_MISSING_NEXT_BLOCK_ID = (
+        'Condition block rule missing next block id'
+    )
+    MISSING_RESULT_BLOCK = 'Flow is missing a result block'
+    RESULT_BLOCK_MUST_CONTAIN_DECISION_VALUE = (
+        'Result block must contain decision value'
+    )
+    INVALID_NEXT_BLOCK_REFERENCE = 'Next_block_id or Next_block_temp_id must reference a valid block in the flow.'
+    FLOW_CONTAINS_CYCLE = 'The decision flow contains a loop.'
+
+    def code(self):
+        return self.name
+
+    def message(self):
+        return self.value
+
+
+class PolicyValidation(BaseModel):
+    is_valid: bool
+    errors: List[FlowValidationError] = []
