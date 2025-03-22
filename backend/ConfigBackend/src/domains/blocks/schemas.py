3adcb0b785788a2e000dffb763267e2ee2e47759
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-22 17:47:35 -0300

    feat(blocks): add blocks  schemas

---
+++ backend/ConfigBackend/src/domains/blocks/schemas.py
@@ -0,0 +1,39 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from src.domains.policies.models import BlockType, ConditionCriteria
+
+
+class BlockRuleSchema(BaseModel):
+    variable_name: str
+    operator: ConditionCriteria
+    value: str
+    next_block_id: Optional[int] = None
+    next_block_temp_id: Optional[int] = None
+
+
+class BlockSchema(BaseModel):
+    id: Optional[int] = None
+    type: BlockType
+    decision_value: Optional[str] = None
+    next_block_id: Optional[int] = None
+    next_block_rules: Optional[List[BlockRuleSchema]] = []
+
+
+class CreateOrUpdateBlockRuleSchema(BaseModel):
+    variable_name: str
+    operator: ConditionCriteria
+    value: Optional[str] = None
+    next_block_id: Optional[int] = None
+    next_block_temp_id: Optional[int] = None
+
+
+class CreateOrUpdateBlockSchema(BaseModel):
+    id: Optional[int] = None
+    temp_id: Optional[int] = None
+    type: BlockType
+    decision_value: Optional[str] = None
+    next_block_id: Optional[int] = None
+    next_block_temp_id: Optional[int] = None
+    next_block_rules: Optional[List[CreateOrUpdateBlockRuleSchema]] = []
