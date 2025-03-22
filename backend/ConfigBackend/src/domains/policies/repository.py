f67f25450ed8bb4c201cf36c387281a964ea024b
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-22 17:45:51 -0300

    feat(policy): add policy repository

---
+++ backend/ConfigBackend/src/domains/policies/repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload, selectinload
+
+from src.domains.policies.models import Block, Policy
+from src.repository import BaseRepository
+
+
+class PolicyRepository(BaseRepository):
+    def __init__(self, db_session):
+        super().__init__(db_session)
+        self.model = Policy
+        self.db_session = db_session
+
+    async def get_by_name(self, name: str) -> Policy:
+        db_policy = await self.db_session.scalar(
+            select(self.model).where(self.model.name == name)
+        )
+
+        return db_policy
+
+    async def get_by_id_with_blocks(self, id: int) -> Policy:
+        db_policy = await self.db_session.scalar(
+            select(self.model).where(self.model.id == id)
+        )
+
+        return db_policy
