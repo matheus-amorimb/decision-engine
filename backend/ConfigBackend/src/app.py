8c8b768fc047dd063c4d063aafdd8a0780cb2955
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-23 17:00:10 -0300

    feat(backend/cors): add cors middleware to allow cross-origin requests

--- backend/ConfigBackend/src/app.py
+++ backend/ConfigBackend/src/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.domains.policies.router import router as policies_router
 from src.exceptions import BaseAppException
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(policies_router)
 
