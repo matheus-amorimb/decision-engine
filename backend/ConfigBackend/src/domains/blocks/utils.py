7de41560a368bc392efe1daa1e6a92dbac6a5d81
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-26 19:55:17 -0300

    chore(back/block): add utils methods to serialize blocks

--- backend/ConfigBackend/src/domains/blocks/utils.py
+++ backend/ConfigBackend/src/domains/blocks/utils.py
@@ -1,6 +1,15 @@
 from typing import List
 
-from src.domains.blocks.schemas import BlockRuleSchema, BlockSchema
+from ConfigBackend.src.utils.string_utils import (
+    convert_underscores_to_spaces,
+    convert_spaces_to_underscores,
+)
+from src.domains.blocks.schemas import (
+    BlockRuleSchema,
+    BlockSchema,
+    CreateOrUpdateBlockRuleSchema,
+    CreateOrUpdateBlockSchema,
+)
 from src.domains.policies.models import Block, BlockRule
 
 
@@ -14,13 +23,48 @@ def block_model_to_schema(block: Block) -> BlockSchema:
             block_rule_model_to_schema(block_rule)
             for block_rule in block.next_block_rules
         ],
+        position_x=block.position_x,
+        position_y=block.position_y,
     )
 
 
 def block_rule_model_to_schema(block_rule: BlockRule) -> BlockRuleSchema:
     return BlockRuleSchema(
-        variable_name=block_rule.variable_name,
+        variable_name=convert_underscores_to_spaces(block_rule.variable_name),
         operator=block_rule.operator,
         value=block_rule.value,
         next_block_id=block_rule.next_block_id,
+    )
+
+
+def block_schemas_to_entities(
+    policy_id: int, flow: List[CreateOrUpdateBlockSchema]
+) -> List[Block]:
+    return [
+        create_or_updated_block_schema_to_entity(policy_id, block_schema)
+        for block_schema in flow
+    ]
+
+
+def create_or_updated_block_schema_to_entity(
+    policy_id: int, block: CreateOrUpdateBlockSchema
+) -> Block:
+    return Block(
+        type=block.type,
+        policy_id=policy_id,
+        decision_value=block.decision_value,
+        position_y=block.position_y,
+        position_x=block.position_x,
+    )
+
+
+def create_or_update_block_rule_schema_to_entity(
+    current_block_id, next_block_id, block_rule: CreateOrUpdateBlockRuleSchema
+) -> BlockRule:
+    return BlockRule(
+        variable_name=convert_spaces_to_underscores(block_rule.variable_name),
+        current_block_id=current_block_id,
+        next_block_id=next_block_id,
+        operator=block_rule.operator,
+        value=block_rule.value,
     )
