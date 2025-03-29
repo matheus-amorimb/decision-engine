6baada4740602f099759ac54f4fc71d3b1fee08d
Matheus Batista <mbatista.sarti@gmail.com>
2025-03-29 19:28:33 -0300

    refactor(frontend/policy): refactor block handling logic in buildNodesAndEdgesFromPolicy

--- frontend/src/utils/policy.ts
+++ frontend/src/utils/policy.ts
@@ -1,10 +1,10 @@
-import { BlocksType } from "@src/components/Blocks";
-import { ConditionBlockData } from "@src/components/Blocks/ConditionBlock";
-import { ResultBlockData } from "@src/components/Blocks/ResultBlock";
-import { StartBlockData } from "@src/components/Blocks/StartBlock";
-import { BlockRuleProtocol, CreateOrUpdateBlockProtocol, CreateOrUpdateBlockRuleProtocol, PolicyProtocol } from "@src/protocols/policy";
-import { nanoid } from "nanoid";
-import { Node, Edge, MarkerType } from "reactflow";
+import { BlocksType } from "@src/components/Blocks"
+import { ConditionBlockData } from "@src/components/Blocks/ConditionBlock"
+import { ResultBlockData } from "@src/components/Blocks/ResultBlock"
+import { StartBlockData } from "@src/components/Blocks/StartBlock"
+import { BlockProtocol, BlockRuleProtocol, CreateOrUpdateBlockProtocol, CreateOrUpdateBlockRuleProtocol, PolicyProtocol } from "@src/protocols/policy"
+import { nanoid } from "nanoid"
+import { Node, Edge, MarkerType } from "reactflow"
 
 export const buildPolicyFlow = (nodes: Node[], edges: Edge[]): CreateOrUpdateBlockProtocol[]  => {
   let blocks: CreateOrUpdateBlockProtocol[] = []
@@ -129,138 +129,149 @@ const buildConditionRules = (conditionNode: Node<ConditionBlockData>, edges: Edg
   return [conditionRule, elseRule]
 }
 
-export const buildNodesAndEdgesFromPolicy = (policy: PolicyProtocol): { nodes: Node[], edges: Edge[]} => {
+const handleStartBlock = (
+  block: BlockProtocol, 
+  policyName: string, 
+  idToTempId: Record<number, string>, 
+  nodes: Node[], 
+  edges: Edge[]
+) => {
+  let nextBlockTempId = idToTempId[block.next_block_id] || generateUniqueNodeId()
+
+  // If the next_block_id does not already have a tempId, add it to the map
+  if (!idToTempId[block.next_block_id]) {
+    idToTempId[block.next_block_id] = nextBlockTempId
+  }
+
+  const nodeTempId = generateUniqueNodeId()
+  const startNode: Node<StartBlockData> = {
+    id: nodeTempId,
+    type: "start",
+    position: { x: block.position_x, y: block.position_y },
+    data: {
+      title: policyName,
+      next_block_temp_id: nextBlockTempId
+    }
+  }
+
+  const edge: Edge = {
+    id: generateUniqueNodeId(),
+    source: nodeTempId,
+    target: nextBlockTempId,
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 15,
+      height: 15,
+      color: "#000"
+    }
+  }
+
+  nodes.push(startNode)
+  edges.push(edge)
+}
+
+const handleConditionBlock = (
+  block: BlockProtocol,
+  idToTempId: Record<number, string>,
+  nodes: Node[],
+  edges: Edge[]
+) => {
+  const nodeTempId = idToTempId[block.id] || generateUniqueNodeId()
+  let trueRule: BlockRuleProtocol = {
+    variable_name: "",
+    operator: "<",
+    value: "",
+    next_block_id: 0,
+    next_block_temp_id: ""
+  }
+
+  block.next_block_rules.forEach((rule) => {
+    let nextBlockTempId = idToTempId[rule.next_block_id] || generateUniqueNodeId()
+    if (!idToTempId[rule.next_block_id]) {
+      idToTempId[rule.next_block_id] = nextBlockTempId
+    }
+
+    const edge: Edge = {
+      id: generateUniqueNodeId(),
+      source: nodeTempId,
+      target: nextBlockTempId,
+      sourceHandle: rule.operator === "else" ? "source-else" : "source-condition",
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
+        width: 15,
+        height: 15,
+        color: "#000"
+      }
+    }
+
+    edges.push(edge)
+
+    if (rule.operator !== "else") {
+      trueRule = rule
+    }
+  })
+
+  const conditionNode: Node<ConditionBlockData> = {
+    id: nodeTempId,
+    type: "condition",
+    position: { x: block.position_x, y: block.position_y },
+    data: {
+      rule: {
+        variableName: trueRule.variable_name,
+        operator: trueRule.operator,
+        value: trueRule.value,
+        next_block_temp_id: trueRule.next_block_temp_id
+      }
+    }
+  }
+
+  idToTempId[block.id] = nodeTempId
+
+  nodes.push(conditionNode)
+}
+
+const handleResultBlock = (
+  block: BlockProtocol,
+  idToTempId: Record<number, string>,
+  nodes: Node[]
+) => {
+  const tempId = idToTempId[block.id] || generateUniqueNodeId()
+  const resultNode: Node<ResultBlockData> = {
+    id: tempId,
+    type: "result",
+    position: { x: block.position_x, y: block.position_y },
+    data: {
+      decision_value: block.decision_value
+    }
+  }
+
+  idToTempId[block.id] = tempId
+
+  nodes.push(resultNode)
+}
+
+export const buildNodesAndEdgesFromPolicy = (policy: PolicyProtocol): { nodes: Node[], edges: Edge[] } => {
   let nodes: Node[] = []
   let edges: Edge[] = []
   let idToTempId: Record<number, string> = {}
-  
+
   policy.flow.forEach((block) => {
-    const nodePosition = { x: block.position_x, y: block.position_y }
-    
-    /*
-      For START BLOCK:
-      - Every start block has an edge.
-    */
-    
-    if (block.type === "start") {
-      let nextBlockTempId = idToTempId[block.next_block_id] || generateUniqueNodeId();
+    switch (block.type) {
+      case "start":
+        handleStartBlock(block, policy.name, idToTempId, nodes, edges)
+        break
 
-      // If the next_block_id does not already have a tempId, add it to the map
-      if (!idToTempId[block.next_block_id]) {
-        idToTempId[block.next_block_id] = nextBlockTempId;
-      }
+      case "condition":
+        handleConditionBlock(block, idToTempId, nodes, edges)
+        break
 
-      // Since a start block has only one outgoing edge, there's no need to
-      // check if its ID exists in the map before assigning it.
-      const nodeTempId = generateUniqueNodeId()
-      const startNode: Node<StartBlockData> = {
-        id: nodeTempId,
-        type: "start",
-        position: nodePosition,
-        data: {
-          title: policy.name,
-          next_block_temp_id: nextBlockTempId
-        }
-      }
+      case "result":
+        handleResultBlock(block, idToTempId, nodes)
+        break
 
-      const edge: Edge = {
-        id: generateUniqueNodeId(),
-        source: nodeTempId,
-        target: nextBlockTempId,
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-          width: 15,
-          height: 15,
-          color: "#000"
-        }
-      }
-
-      nodes.push(startNode)
-      edges.push(edge)
-    }
-
-    if (block.type === "condition") {
-      const nodeTempId = idToTempId[block.id] || generateUniqueNodeId() 
-
-      let trueRule: BlockRuleProtocol = {
-        variable_name: "",
-        operator: "<",
-        value: "",
-        next_block_id: 0,
-        next_block_temp_id: ""
-      }
-      block.next_block_rules.forEach((rule) => {
-
-        let nextBlockTempId = idToTempId[rule.next_block_id] || generateUniqueNodeId()
-        if (!idToTempId[rule.next_block_id]) {
-          idToTempId[rule.next_block_id] = nextBlockTempId;
-        }
-
-        if (rule.operator === "else") {
-          const edge: Edge = {
-            id: generateUniqueNodeId(),
-            source: nodeTempId,
-            target: nextBlockTempId,
-            sourceHandle: "source-else",
-            markerEnd: {
-              type: MarkerType.ArrowClosed,
-              width: 15,
-              height: 15,
-              color: "#000"
-            }
-          }
-          edges.push(edge)
-        } else {
-          trueRule = rule
-          const edge: Edge = {
-            id: generateUniqueNodeId(),
-            source: nodeTempId,
-            target: nextBlockTempId,
-            sourceHandle: "source-condition",
-            markerEnd: {
-              type: MarkerType.ArrowClosed,
-              width: 15,
-              height: 15,
-              color: "#000"
-            }
-          }
-          edges.push(edge)
-        }
-      })
-
-      const conditionNode: Node<ConditionBlockData> = {
-        id: nodeTempId,
-        type: "condition",
-        position: nodePosition,
-        data: {
-          rule: {
-            variableName: trueRule.variable_name,
-            operator: trueRule.operator,
-            value: trueRule.value,
-            next_block_temp_id: trueRule.next_block_temp_id
-          }
-        }
-      }
-
-      idToTempId[block.id] = nodeTempId
-
-      nodes.push(conditionNode)
-    }
-    
-    if (block.type === "result") {
-      const tempId = idToTempId[block.id] || generateUniqueNodeId() 
-      const resultNode: Node<ResultBlockData> = {
-        id: tempId,
-        type: "result",
-        position: nodePosition,
-        data: {
-          decision_value: block.decision_value
-        }
-      }
-      
-      idToTempId[block.id] = tempId
-
-      nodes.push(resultNode)
+      default:
+        console.error(`Unknown block type: ${block.type}`)
+        break
     }
   })
 
@@ -271,12 +282,12 @@ export const buildNodesAndEdgesFromPolicy = (policy: PolicyProtocol): { nodes: N
 }
 
 export const generateUniqueNodeId = () => {
-  const arbitraryButShortIdLength = 10;
-  return nanoid(arbitraryButShortIdLength);
+  const arbitraryButShortIdLength = 10
+  return nanoid(arbitraryButShortIdLength)
 }
 
 const isOnlyDigits = (str: string): boolean => {
-  return /^\d+$/.test(str);
+  return /^\d+$/.test(str)
 }
 
 const getBlockId = (id: string) => {
